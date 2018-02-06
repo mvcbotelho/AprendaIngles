a1a8566d1d2e356bbac81bba9454fdc2233a1e85
marcusbotelho <marcus.itec@gmail.com>
2018-02-06 19:42:56 -0300

    UPGRADE: Create a shared folder with a phrase model and create a mock for phrases.

---
+++ src/app/panel/prhase-mock.ts
@@ -0,0 +1,8 @@
+import { Phrase } from '../shared/phrase.model'
+
+const PHRASE: Phrase[] = [
+  { phraseEN: 'I like to learn', phrasePTBR: 'Eu gosto de aprender' },
+  { phraseEN: 'I watch tv', phrasePTBR: 'Eu assisto tv' },
+  { phraseEN: 'How are you?', phrasePTBR: 'Como vai você?' },
+  { phraseEN: 'I eat bread', phrasePTBR: 'Eu como pão' }
+];
---
+++ src/app/shared/phrase.model.ts
@@ -0,0 +1,6 @@
+export class Phrase {
+
+  constructor(public phraseEN: string, public phrasePTBR: string){
+    
+  }
+}
