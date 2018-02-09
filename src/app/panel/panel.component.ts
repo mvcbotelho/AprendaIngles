d5e705117df27c6c848fbb9e22f13f20b30e75c0
marcusbotelho <marcus.itec@gmail.com>
2018-02-09 08:48:54 -0300

    UPTADE: Event click add in button for verification

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+import { Phrase } from '../shared/phrase.model';
+import { PHRASE } from './phrase-mock';
+
+ @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css']
 })
 export class PanelComponent implements OnInit {
 
-  constructor() { }
+   public phrase: Phrase[] = PHRASE;
+   public instruction: string = 'Traduza a frase';
+   public response: string;
+  constructor() { console.log(this.phrase) }
 
   ngOnInit() {
   }
 
+  public updateResponse(response: Event): void {
+    this.response = ((<HTMLInputElement>response.target).value);
+    // console.log(this.response);
+  }
+
+  public checkAnswer(): void {
+    console.log('Verifica resposta:', this.response);
+  }
 }
--- src/app/panel/phrase-mock.ts
+++ src/app/panel/phrase-mock.ts
@@ -1,6 +1,6 @@
 import { Phrase } from '../shared/phrase.model'
 
-const PHRASE: Phrase[] = [
+ export const PHRASE: Phrase[] = [
   { phraseEN: 'I like to learn', phrasePTBR: 'Eu gosto de aprender' },
   { phraseEN: 'I watch tv', phrasePTBR: 'Eu assisto tv' },
   { phraseEN: 'How are you?', phrasePTBR: 'Como vai você?' },
